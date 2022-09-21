e10e0ca36c0c13fca60e908316fc0be1ac22ac5f
haldiespasya <81854080+haldies@users.noreply.github.com>
2022-09-21 08:12:49 +0700

    Add files via upload

---
+++ App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat mudah sekali");
+        System.out.println("dan sangat menyenangkan");
+    }
+}
